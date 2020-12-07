67be4bd87dc87f45d43bd192d2eac35adf406024
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-07 11:13:48 +0100

    Init the Covid19DataProvider class

---
+++ src/main/java/com/nexthink/mnt/covidalertservice/service/Covid19DataProvider.java
@@ -0,0 +1,13 @@
+package com.nexthink.mnt.covidalertservice.service;
+
+
+import com.nexthink.mnt.covidalertservice.dto.CountryAndStateData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Covid19DataProvider {
+
+    CountryAndStateData getCountryAndStateData(String country, String state) {
+
+    }
+}
