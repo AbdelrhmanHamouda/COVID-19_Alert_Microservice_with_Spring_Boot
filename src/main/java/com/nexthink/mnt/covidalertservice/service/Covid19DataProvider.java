e20db7e93fa8e0df6231b2d457daa1388abfabb8
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-07 18:10:08 +0100

    Implement JSON parsing logic to getCountryAndStateData()

--- src/main/java/com/nexthink/mnt/covidalertservice/service/Covid19DataProvider.java
+++ src/main/java/com/nexthink/mnt/covidalertservice/service/Covid19DataProvider.java
@@ -1,7 +1,10 @@
 package com.nexthink.mnt.covidalertservice.service;
 
 
-import com.nexthink.mnt.covidalertservice.dto.CountryAndStateData;
+import jsonij.Value;
+import jsonij.parser.JSONParser;
+import jsonij.parser.ParserException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,10 +12,47 @@ import org.springframework.web.client.RestTemplate;
 public class Covid19DataProvider {
 
     // TODO: The url should be modified with country and state provided
-    final String url = "https://fxs6l35t9e.execute-api.eu-west-3.amazonaws.com/pro/api/2020-12-06/country/spain/region/madrid";
+    String apiUrl = "https://fxs6l35t9e.execute-api.eu-west-3.amazonaws.com/pro/api/2020-12-06/";
 
+    // This will be used to send requests to the api
+    @Autowired
     RestTemplate restTemplate;
-    CountryAndStateData getCountryAndStateData(String country, String state) {
+
+    public String getCountryAndStateData(String country, String state, String dataField) throws ParserException {
+        // Provide a default value for dataField if it was passed as null
+        dataField = dataField != null ? dataField : "today_confirmed";
+        // Init return value
+        Value regionData;
+        // Init the Jason parser
+        JSONParser parser = new JSONParser();
+        // Update URL with selected country and state
+        String countryAndStateEndpoint = "country/" + country + "/region/" + state;
+        // Finalize Request url
+        String requestUrl = apiUrl + countryAndStateEndpoint;
+        // Get the API response and store it in a string
+        String JsonResponse = restTemplate.getForObject(requestUrl, String.class);
+        // ? For debug
+        System.out.println("Response from api url is " + JsonResponse);
+        // Parse the JsonResponse
+        Value jsonBody = parser.parse(JsonResponse);
+        // ? For debug
+        System.out.println("Data after parsing " + jsonBody);
+        // Get region data
+        if (dataField.equals("all")) {
+            // Provide all received data
+            // TODO replace Spain with country
+            regionData = jsonBody.get("dates").get("2020-12-06").get("countries").get(country).get("regions").get(0);
+            // ? For debug
+            System.out.println("Returned data is " + regionData);
+        } else {
+            // Provide specific data
+            // TODO replace Spain with country
+            regionData = jsonBody.get("dates").get("2020-12-06").get("countries").get(country).get("regions").get(0).get(dataField);
+            // ? For debug
+            System.out.println("Returned data is " + regionData);
+        }
+        return String.valueOf(regionData);
+
 
     }
 }
