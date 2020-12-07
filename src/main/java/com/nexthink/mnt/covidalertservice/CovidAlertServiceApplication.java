e6029875b29b981ce79f63ba01e612eda5a49d25
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-07 10:06:38 +0100

    Init project commit

---
+++ src/main/java/com/nexthink/mnt/covidalertservice/CovidAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexthink.mnt.covidalertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAlertServiceApplication.class, args);
+	}
+
+}
