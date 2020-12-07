eea97120477bd4f82c7ff5b4156ad2169dfcfbff
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-07 12:53:05 +0100

    Added expected fields from API. Added Getters and Setters for all.

--- src/main/java/com/nexthink/mnt/covidalertservice/dto/CountryAndStateData.java
+++ src/main/java/com/nexthink/mnt/covidalertservice/dto/CountryAndStateData.java
@@ -1,5 +1,257 @@
 package com.nexthink.mnt.covidalertservice.dto;
 
 public class CountryAndStateData {
+    // Initialize all expected variables
+    int today_confirmed;
+    int today_deaths;
+    int today_hospitalised_patients_with_symptoms;
+    int today_intensive_care;
+    int today_new_confirmed;
+    int today_new_deaths;
+    int today_new_hospitalised_patients_with_symptoms;
+    int today_new_intensive_care;
+    int today_new_open_cases;
+    int today_new_recovered;
+    int today_new_total_hospitalised_patients;
+    int today_open_cases;
+    int today_recovered;
+    int today_total_hospitalised_patients;
+    int today_vs_yesterday_confirmed;
+    int today_vs_yesterday_deaths;
+    int today_vs_yesterday_hospitalised_patients_with_symptoms;
+    int today_vs_yesterday_intensive_care;
+    int today_vs_yesterday_open_cases;
+    int today_vs_yesterday_recovered;
+    int today_vs_yesterday_total_hospitalised_patients;
+    int yesterday_confirmed;
+    int yesterday_deaths;
+    int yesterday_hospitalised_patients_with_symptoms;
+    int yesterday_intensive_care;
+    int yesterday_open_cases;
+    int yesterday_recovered;
+    int yesterday_total_hospitalised_patients;
 
+    public int getToday_confirmed() {
+        return today_confirmed;
+    }
+
+    public void setToday_confirmed(int today_confirmed) {
+        this.today_confirmed = today_confirmed;
+    }
+
+    public int getToday_deaths() {
+        return today_deaths;
+    }
+
+    public void setToday_deaths(int today_deaths) {
+        this.today_deaths = today_deaths;
+    }
+
+    public int getToday_hospitalised_patients_with_symptoms() {
+        return today_hospitalised_patients_with_symptoms;
+    }
+
+    public void setToday_hospitalised_patients_with_symptoms(int today_hospitalised_patients_with_symptoms) {
+        this.today_hospitalised_patients_with_symptoms = today_hospitalised_patients_with_symptoms;
+    }
+
+    public int getToday_intensive_care() {
+        return today_intensive_care;
+    }
+
+    public void setToday_intensive_care(int today_intensive_care) {
+        this.today_intensive_care = today_intensive_care;
+    }
+
+    public int getToday_new_confirmed() {
+        return today_new_confirmed;
+    }
+
+    public void setToday_new_confirmed(int today_new_confirmed) {
+        this.today_new_confirmed = today_new_confirmed;
+    }
+
+    public int getToday_new_deaths() {
+        return today_new_deaths;
+    }
+
+    public void setToday_new_deaths(int today_new_deaths) {
+        this.today_new_deaths = today_new_deaths;
+    }
+
+    public int getToday_new_hospitalised_patients_with_symptoms() {
+        return today_new_hospitalised_patients_with_symptoms;
+    }
+
+    public void setToday_new_hospitalised_patients_with_symptoms(int today_new_hospitalised_patients_with_symptoms) {
+        this.today_new_hospitalised_patients_with_symptoms = today_new_hospitalised_patients_with_symptoms;
+    }
+
+    public int getToday_new_intensive_care() {
+        return today_new_intensive_care;
+    }
+
+    public void setToday_new_intensive_care(int today_new_intensive_care) {
+        this.today_new_intensive_care = today_new_intensive_care;
+    }
+
+    public int getToday_new_open_cases() {
+        return today_new_open_cases;
+    }
+
+    public void setToday_new_open_cases(int today_new_open_cases) {
+        this.today_new_open_cases = today_new_open_cases;
+    }
+
+    public int getToday_new_recovered() {
+        return today_new_recovered;
+    }
+
+    public void setToday_new_recovered(int today_new_recovered) {
+        this.today_new_recovered = today_new_recovered;
+    }
+
+    public int getToday_new_total_hospitalised_patients() {
+        return today_new_total_hospitalised_patients;
+    }
+
+    public void setToday_new_total_hospitalised_patients(int today_new_total_hospitalised_patients) {
+        this.today_new_total_hospitalised_patients = today_new_total_hospitalised_patients;
+    }
+
+    public int getToday_open_cases() {
+        return today_open_cases;
+    }
+
+    public void setToday_open_cases(int today_open_cases) {
+        this.today_open_cases = today_open_cases;
+    }
+
+    public int getToday_recovered() {
+        return today_recovered;
+    }
+
+    public void setToday_recovered(int today_recovered) {
+        this.today_recovered = today_recovered;
+    }
+
+    public int getToday_total_hospitalised_patients() {
+        return today_total_hospitalised_patients;
+    }
+
+    public void setToday_total_hospitalised_patients(int today_total_hospitalised_patients) {
+        this.today_total_hospitalised_patients = today_total_hospitalised_patients;
+    }
+
+    public int getToday_vs_yesterday_confirmed() {
+        return today_vs_yesterday_confirmed;
+    }
+
+    public void setToday_vs_yesterday_confirmed(int today_vs_yesterday_confirmed) {
+        this.today_vs_yesterday_confirmed = today_vs_yesterday_confirmed;
+    }
+
+    public int getToday_vs_yesterday_deaths() {
+        return today_vs_yesterday_deaths;
+    }
+
+    public void setToday_vs_yesterday_deaths(int today_vs_yesterday_deaths) {
+        this.today_vs_yesterday_deaths = today_vs_yesterday_deaths;
+    }
+
+    public int getToday_vs_yesterday_hospitalised_patients_with_symptoms() {
+        return today_vs_yesterday_hospitalised_patients_with_symptoms;
+    }
+
+    public void setToday_vs_yesterday_hospitalised_patients_with_symptoms(int today_vs_yesterday_hospitalised_patients_with_symptoms) {
+        this.today_vs_yesterday_hospitalised_patients_with_symptoms = today_vs_yesterday_hospitalised_patients_with_symptoms;
+    }
+
+    public int getToday_vs_yesterday_intensive_care() {
+        return today_vs_yesterday_intensive_care;
+    }
+
+    public void setToday_vs_yesterday_intensive_care(int today_vs_yesterday_intensive_care) {
+        this.today_vs_yesterday_intensive_care = today_vs_yesterday_intensive_care;
+    }
+
+    public int getToday_vs_yesterday_open_cases() {
+        return today_vs_yesterday_open_cases;
+    }
+
+    public void setToday_vs_yesterday_open_cases(int today_vs_yesterday_open_cases) {
+        this.today_vs_yesterday_open_cases = today_vs_yesterday_open_cases;
+    }
+
+    public int getToday_vs_yesterday_recovered() {
+        return today_vs_yesterday_recovered;
+    }
+
+    public void setToday_vs_yesterday_recovered(int today_vs_yesterday_recovered) {
+        this.today_vs_yesterday_recovered = today_vs_yesterday_recovered;
+    }
+
+    public int getToday_vs_yesterday_total_hospitalised_patients() {
+        return today_vs_yesterday_total_hospitalised_patients;
+    }
+
+    public void setToday_vs_yesterday_total_hospitalised_patients(int today_vs_yesterday_total_hospitalised_patients) {
+        this.today_vs_yesterday_total_hospitalised_patients = today_vs_yesterday_total_hospitalised_patients;
+    }
+
+    public int getYesterday_confirmed() {
+        return yesterday_confirmed;
+    }
+
+    public void setYesterday_confirmed(int yesterday_confirmed) {
+        this.yesterday_confirmed = yesterday_confirmed;
+    }
+
+    public int getYesterday_deaths() {
+        return yesterday_deaths;
+    }
+
+    public void setYesterday_deaths(int yesterday_deaths) {
+        this.yesterday_deaths = yesterday_deaths;
+    }
+
+    public int getYesterday_hospitalised_patients_with_symptoms() {
+        return yesterday_hospitalised_patients_with_symptoms;
+    }
+
+    public void setYesterday_hospitalised_patients_with_symptoms(int yesterday_hospitalised_patients_with_symptoms) {
+        this.yesterday_hospitalised_patients_with_symptoms = yesterday_hospitalised_patients_with_symptoms;
+    }
+
+    public int getYesterday_intensive_care() {
+        return yesterday_intensive_care;
+    }
+
+    public void setYesterday_intensive_care(int yesterday_intensive_care) {
+        this.yesterday_intensive_care = yesterday_intensive_care;
+    }
+
+    public int getYesterday_open_cases() {
+        return yesterday_open_cases;
+    }
+
+    public void setYesterday_open_cases(int yesterday_open_cases) {
+        this.yesterday_open_cases = yesterday_open_cases;
+    }
+
+    public int getYesterday_recovered() {
+        return yesterday_recovered;
+    }
+
+    public void setYesterday_recovered(int yesterday_recovered) {
+        this.yesterday_recovered = yesterday_recovered;
+    }
+
+    public int getYesterday_total_hospitalised_patients() {
+        return yesterday_total_hospitalised_patients;
+    }
+
+    public void setYesterday_total_hospitalised_patients(int yesterday_total_hospitalised_patients) {
+        this.yesterday_total_hospitalised_patients = yesterday_total_hospitalised_patients;
+    }
 }
